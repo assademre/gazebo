eb26a92b49ab78b375f0bce5f10e89e4939fde53
Emre Yılmaz <assademre@gmail.com>
2024-04-11 14:12:32 +0200

    typing effect added to splash screen

--- Gazebo/frontend-app/src/AccountManagement/SplashScreen/Splash.js
+++ Gazebo/frontend-app/src/AccountManagement/SplashScreen/Splash.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Splash.css';
 import logo from "../../logo_black.png";
 
 const Splash = () => {
     const navigate = useNavigate();
+    const [typedText, setTypedText] = useState('');
+    const fullText = `Nestled in a bustling city, "The Serene Bean" cafe offers cozy ambiance, aromatic coffee, and delicious pastries. Patrons enjoy quiet moments or lively conversations amidst the hustle and bustle. It's a sanctuary where worries fade and friendships flourish over steaming cups of coffee.`;
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const typingInterval = setInterval(() => {
+            if (index < fullText.length) {
+                setTypedText((prev) => prev + fullText[index]);
+                setIndex((prev) => prev + 1);
+            }
+        }, 20);
+
+        return () => clearInterval(typingInterval);
+    }, [index, fullText]);
 
     const handleLoginClick = () => {
         navigate('/login');
@@ -18,8 +32,7 @@ const Splash = () => {
         <div className="splash-screen">
             <img src={logo} alt="Logo" className="logo-splash" />
             <div className="splash-content">
-                <p className='text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut urna vel eros euismod malesuada. Nulla facilisi. Integer tempus elit nec sapien convallis, sed faucibus felis bibendum. Suspendisse potenti. Nunc ut libero a justo scelerisque posuere. Sed malesuada sapien at ipsum elementum vestibulum. Nam auctor odio eget velit venenatis fermentum. Integer pretium elit vitae nisi vehicula, a bibendum arcu bibendum. Morbi pulvinar velit nec urna tincidunt, eget eleifend erat varius. Nam vitae libero et justo luctus tristique. Nulla sollicitudin risus
-</p>
+                <p className='text typing-animation'>{typedText}</p>
                 <div className="button-container">
                     <button className="login-button" onClick={handleLoginClick}>Login</button>
                     <button className="signup-button" onClick={handleSignupClick}>Signup</button>
