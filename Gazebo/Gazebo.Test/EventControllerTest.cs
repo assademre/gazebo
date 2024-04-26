9e25461926db231480ee586b78a3729cb8261368
Emre Yılmaz <assademre@gmail.com>
2024-04-26 15:32:33 +0200

    Unit test for event controller added

---
+++ Gazebo/Gazebo.Test/EventControllerTest.cs
@@ -0,0 +1,128 @@
+using Moq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using EventOrganizationApp.Controller;
+using EventOrganizationApp.Data.Dto;
+using EventOrganizationApp.Models;
+using Gazebo.Interfaces;
+using EventOrganizationApp.Models.Enums;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Gazebo.Test
+{
+    [TestFixture]
+    public class EventControllerTests
+    {
+        private EventController _controller;
+        private Mock<IEventRepository> _eventRepositoryMock;
+        private Mock<IEventMemberRepository> _eventMemberRepositoryMock;
+        private Mock<IMapper> _mapperMock;
+        private const int TestUserId = 1;
+
+        [SetUp]
+        public void Setup()
+        {
+            _eventRepositoryMock = new Mock<IEventRepository>();
+            _eventMemberRepositoryMock = new Mock<IEventMemberRepository>();
+            _mapperMock = new Mock<IMapper>();
+
+            _eventRepositoryMock.Setup(repo => repo.GetEventIdByEventNameAndUserId(It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync(1);
+            _eventMemberRepositoryMock.Setup(repo => repo.AddEventMember(It.IsAny<EventMember>())).ReturnsAsync(true);
+
+            _controller = new EventController(_eventRepositoryMock.Object, _mapperMock.Object, _eventMemberRepositoryMock.Object);
+
+            SetupHttpContext();
+
+        }
+
+        private void SetupHttpContext()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("userId", TestUserId.ToString())
+            }));
+
+            var controllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            _controller.ControllerContext = controllerContext;
+        }
+
+        [Test]
+        public async Task GetEventUserCreated_ReturnsOkResult()
+        {
+            var events = GetTestEvents();
+            var mappedEvents = MapEventsToDto(events);
+
+            _eventRepositoryMock.Setup(repo => repo.GetEventsUserCreated(TestUserId)).ReturnsAsync(events);
+            _mapperMock.Setup(m => m.Map<IList<EventDto>>(events)).Returns(mappedEvents);
+            var result = await _controller.GetEventUserCreated();
+
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task CreateEvent_ReturnsOkResult()
+        {
+            var newEvent = GetTestEventDto();
+            var mappedEvent = MapEventDtoToEvent(newEvent);
+
+            _mapperMock.Setup(m => m.Map<Event>(newEvent)).Returns(mappedEvent);
+            _eventRepositoryMock.Setup(repo => repo.CreateEvent(mappedEvent)).ReturnsAsync(true);
+            var result = await _controller.CreateEvent(newEvent);
+
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        private List<Event> GetTestEvents()
+        {
+            return new List<Event>
+            {
+                new Event
+                {
+                    CreaterId = TestUserId,
+                    EventName = "Event Test",
+                    EventTypeId = (int)EventType.Default,
+                    Budget = 100,
+                    CurrencyId = (int)CurrencyModel.GBP,
+                    Place = "Place Test",
+                    StatusId = (int)Status.NotStarted,
+                    CreatedDate = DateTime.UtcNow,
+                    UpdatedDate = DateTime.UtcNow,
+                    EventDate = DateTime.UtcNow
+                }
+            };
+        }
+
+        private List<EventDto> MapEventsToDto(List<Event> events)
+        {
+            return events.Select(e => _mapperMock.Object.Map<EventDto>(e)).ToList();
+        }
+
+        private EventDto GetTestEventDto()
+        {
+            return new EventDto
+            {
+                CreaterId = TestUserId,
+                EventName = "Test Event",
+                EventType = EventType.Default.ToString(),
+                Budget = 200,
+                Currency = CurrencyModel.EUR.ToString(),
+                Place = "Testland",
+                Status = Status.InProgress.ToString(),
+                CreatedDate = DateTime.UtcNow,
+                UpdatedDate = DateTime.UtcNow,
+                EventDate = DateTime.UtcNow
+            };
+        }
+
+        private Event MapEventDtoToEvent(EventDto eventDto)
+        {
+            return _mapperMock.Object.Map<Event>(eventDto);
+        }
+    }
+}
--- Gazebo/Gazebo/Data/Mapper/MappingProfiles.cs
+++ Gazebo/Gazebo/Data/Mapper/MappingProfiles.cs
@@ -13,7 +13,7 @@ namespace EventOrganizationApp.Data.Dto.Mapper
             CreateMap<Event, EventDto>()
                 .ForMember(dest => dest.EventType, opt => opt.MapFrom(src => (EventType)src.EventTypeId))
                 .ForMember(dest => dest.Currency, opt => opt.MapFrom(src => (CurrencyModel)src.CurrencyId))
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => Enum.GetName(typeof(Status), src.StatusId)));
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (Status)src.StatusId));
 
             CreateMap<EventDto, Event>()
                 .ForMember(dest => dest.EventTypeId, opt => opt.MapFrom(src => (int)Enum.Parse(typeof(EventType), src.EventType)))
